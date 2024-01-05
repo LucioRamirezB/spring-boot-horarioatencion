9a321543e4e92d4efbfc306ab7366f5c6f384356
Lucio-RamirezB <lucio.ramirez@coppel.com>
2024-01-05 11:06:29 -0600

    Primer commit para agregar el proyecto al repositorio

---
+++ src/main/java/com/bolsadeideas/springboot/horariointerceptor/app/SpringBootHorarioatencionApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.horariointerceptor.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHorarioatencionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHorarioatencionApplication.class, args);
+	}
+
+}
